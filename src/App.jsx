d16a0d0148c9b9850f2a2bb9cf64804a7d96c106
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-16 23:50:00 +0530

    add appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
 import "./App.css";
+import Navigation from "./components/Navigation";
+<components></components>
 const App = () => {
     return (
         <div>
-            
+            <Navigation/>
         </div>
     );
 };
