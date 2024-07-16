eb39e9b008d60e063b7c52158cd7c3e584f96ea3
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-16 23:39:08 +0530

    modify app,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,7 @@ import "./App.css";
 const App = () => {
     return (
         <div>
-            <nav>
-                <div className="logo">
-                    <img src="brand.png" alt="logo" />
-                </div>
-                <ul>
-                    <li href="#">Menu</li>
-                    <li href="#">Loction</li>
-                    <li href="#">About</li>
-                    <li href="#">Contact</li>
-                </ul>
-
-                <button>Login</button>
-            </nav>
+            
         </div>
     );
 };
