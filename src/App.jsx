6e3ac356fe9864966b8dc009bacceda0ee23a4b9
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-16 22:21:03 +0530

    add application file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+    return (
+        <div>
+            <nav>
+                <div className="logo">
+                    <img src="brand.png" alt="logo" />
+                </div>
+                <ul>
+                    <li href="#">Menu</li>
+                    <li href="#">Loction</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default App;
